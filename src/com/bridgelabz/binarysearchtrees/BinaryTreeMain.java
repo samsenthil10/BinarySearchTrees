09fb0cf64b2c300fc9c941a4bb1174a2024fa4ac
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-02 06:27:00 +0530

    Modified: To show how to create a simple binary search tree

--- src/com/bridgelabz/binarysearchtrees/BinaryTreeMain.java
+++ src/com/bridgelabz/binarysearchtrees/BinaryTreeMain.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.binarysearchtrees;
 
 public class BinaryTreeMain {
-	
 	public static void main(String[] args) {
 		
+		MyBinaryNode<Integer> myBinaryTree = new MyBinaryNode<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
 	}
 }
